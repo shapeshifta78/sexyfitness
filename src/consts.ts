91239cad0d0430c41f769a95482f3066b85223e2
thomas.horster <thomas.horster@check24.de>
2025-03-13 11:58:26 +0100

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sexy Fitness';
-export const SITE_DESCRIPTION = 'sexyfitness.de - Dein Fitnessblog';
+export const SITE_TITLE = "Sexy Fitness";
+export const SITE_DESCRIPTION = "sexyfitness.de - Dein Fitnessblog";
